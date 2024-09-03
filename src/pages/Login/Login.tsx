237303a1aef36e52b25506874b105191bdf98736
Allanbm100 <allan.bm.100@gmail.com>
2024-09-02 22:49:10 -0300

    Aula 02-09

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { Button } from "../../components/Button/Button";
+import { Input } from "../../components/Input/Input";
+import { Layout } from "../../components/Layout/Layout";
+
+export default function Login() {
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLogin(event?.target.value);
+  };
+
+  const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleClick = () => {
+    console.log(login, password)
+  }
+
+  return (
+    <Layout>
+      <h1>Login</h1>
+      <form>
+        <Input
+          type="text"
+          id="login"
+          name="login"
+          label="Login"
+          onChange={handleLogin}
+        />
+
+        <Input
+          type="password"
+          id="password"
+          name="password"
+          label="Senha"
+          onChange={handlePassword}
+        />
+
+        <Button type="button" onClick={handleClick}>
+          Login
+        </Button> {/* Esse elemento tem que retornar algo por usar informações da página (ou apenas o event) */}
+      </form>
+    </Layout>
+  )
+}
