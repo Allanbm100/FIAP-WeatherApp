696a8d017ad3c7e9f56e5b5a468e4f97af8db052
Allan Brito <allan.bm.100@gmail.com>
2024-09-05 21:25:42 -0300

    Aula 05/09

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,24 +1,46 @@
 import { useEffect, useState } from "react";
 import { Layout } from "../../components/Layout/Layout";
-import { cityMock } from "../../mocks/cityMock"
+// import { cityMock } from "../../mocks/cityMock"
 
 export default function Home() {
   {/* Esse 'export default' serve para exportarmos uma página completa */ }
 
-  const [city, setCity] = useState();
+  const [city, setCity] = useState([]);
+
+  const LoadCities = () => {
+    // GET - Modelo da API
+    fetch('https://brasilapi.com.br/api/cptec/v1/cidade') //Aqui fizemos uma requisição
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+
+      .then((data) => {
+        console.log(data);
+        setCity(data);
+      })
+  }
+
   useEffect(() => {
-    setCity(cityMock);
-  }, [])
-  
+    // setCity(cityMock);
+    // console.log("useEffect");
+
+    // if (city.length === 0) {
+    //   return;
+    // }
+
+    LoadCities();
+  }, []);
 
   return (
     <Layout>
       <h1>Home</h1>
-
-      <p>
-        {city?.cidade} / {city?.estado} {/* O '?' (nullish) serve como um 'try', se caso o dado não existir, ele não utiliza */}
-      </p>
-
+      {city.map((item: any) => (
+        <p key={item.id}>
+          {item?.nome} / {item?.estado} {/* O '?' (nullish) serve como um 'try', se caso o dado não existir, ele não utiliza */ }
+        </p>
+      ))}
     </Layout>
   );
-};
+}
+
